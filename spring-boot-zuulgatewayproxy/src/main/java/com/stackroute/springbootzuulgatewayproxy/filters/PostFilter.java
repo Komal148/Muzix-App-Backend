fa4e94c7bbadc141d40017bacaf51f41ca0d5706
Akshay <akshayvb66@gmail.com>
2019-01-28 15:41:55 +0530

    Added Zuul api gateway

---
+++ spring-boot-zuulgatewayproxy/src/main/java/com/stackroute/springbootzuulgatewayproxy/filters/PostFilter.java
@@ -0,0 +1,28 @@
+package com.stackroute.springbootzuulgatewayproxy.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+public class PostFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        System.out.println("Inside response filter");
+        return null;
+    }
+}
