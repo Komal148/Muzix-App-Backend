83f5a06ab77a4c99268c950a4892d7188607f2a0
sunidhi.prasad <sunidhi.prasad@boeing.com>
2019-01-28 14:48:47 +0530

    Changed pom.xml dependencies for Eureka server

--- User Service/src/test/java/com/stackroute/repository/UserRepositoryTest.java
+++ User Service/src/test/java/com/stackroute/repository/UserRepositoryTest.java
@@ -8,13 +8,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UserRepositoryTest {
 
     @Autowired
